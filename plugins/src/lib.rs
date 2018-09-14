484e89256925eabe9cee7416c6ac00625b1bed85
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-09-15 01:23:32 +0200

    export types

--- plugins/src/lib.rs
+++ plugins/src/lib.rs
@@ -2,10 +2,11 @@ pub extern crate libloading;
 pub extern crate uuid;
 #[macro_use]
 extern crate error_chain;
-
 pub mod error;
 pub mod macros;
 pub mod types;
+
+pub use types::*;
 
 #[cfg(test)]
 mod tests {
